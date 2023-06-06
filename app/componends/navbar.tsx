33c673f6146b1d127dd540ed758fdd2546bde336
Lexi4eg <felix.prattes@gmail.com>
2023-06-06 11:22:09 +0200

    navbar fix

--- app/componends/navbar.tsx
+++ app/componends/navbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import { Montserrat } from "next/font/google";
-import {MdCoffeeMaker} from "react-icons/md";
+import { MdCoffeeMaker } from "react-icons/md";
 
 import { BiCoffee } from "react-icons/bi";
 
@@ -15,7 +15,9 @@ function Navbar() {
       <div className=" flex flex-row bg-bg_back text-black justify-between">
         <div className="flex justify-center items-center p-5 text-2xl ">
           <div className="flex text-4xl ">
-            <Link href={"/"}><BiCoffee /></Link>
+            <Link href={"/"}>
+              <BiCoffee />
+            </Link>
           </div>
         </div>
         <div className="flex flex-row justify-center p-4 items-center text-black text-2xl">
@@ -33,14 +35,16 @@ function Navbar() {
               <div className="p-4">
                 <Link href={"/Flat"}>Flat White</Link>
               </div>
+              <div className="p-4">
+                <Link href={"/Order"}>Order</Link>
+              </div>
             </div>
           </div>
         </div>
-        <div className="flex justify-center items-center text-4xl m-4">
-        </div>
+        <div className="flex justify-center items-center text-4xl m-4"></div>
       </div>
     </>
   );
 }
 
-export default Navbar;
+export default Navbar;
